13a0f1d454f3c523f45655cae338441136dfbdcd
Jiayi Wei <MrVPlusOne@gmail.com>
2022-02-04 15:10:58 -0600

    Update documentation.

--- src/SmartAsserts.jl
+++ src/SmartAsserts.jl
@@ -11,11 +11,6 @@ Evaluate a boolean expression, and if it is false, also try to show additional i
 to explain why it's false.
 
 Returns `(value::Bool, reason::Union{Nothing, String})`
-
-Currently, additional information will be returned for the following types of expressions:
-- function calls: e.g., `a <= b`, `bool_f(a,k1=...; k2...)`
-- type asserts: e.g., `Type1 <: Type2`
-- comparison expressions: e.g., `a == b <= c + d <= e`
 """
 macro show_assertion(ex)
     _show_assertion(ex)
@@ -137,15 +132,27 @@ end
 Like @assert, but try to also print out additional information about the arguments.
 Note that each argument is only evaluated once, so there is no extra overhead compared 
 to a normal assert.
+
+Currently, additional information will be returned for the following types of expressions:
+
+- function calls: e.g., `a <= b`, `bool_f(a,k1=...; k2...)`
+
+- type asserts: e.g., `Type1 <: Type2`
+
+- comparison expressions: e.g., `a == b <= c + d <= e`
+
 ## Examples
 ```julia
-julia> let a=4, b=2; @smart_assert(a < b, "Some extra info.") end
-
-ERROR: LoadError: AssertionError: Some extra info. | Caused by: Condition `a < b` failed due to:
-        `a` evaluates to 4
-        `b` evaluates to 2
+julia> let a = 1.0, rtol=0.1
+           @smart_assert isapprox(a, sin(a), atol=0.05; rtol)
+       end
+ERROR: AssertionError: Condition `isapprox(a, sin(a), atol = 0.05; rtol)` failed due to:
+        `a` evaluates to 1.0
+        `sin(a)` evaluates to 0.8414709848078965
+        `0.05` evaluates to 0.05
+        `rtol` evaluates to 0.1
 Stacktrace:
-...
+ ...
 ```
 """
 macro smart_assert(ex, msg=nothing)
